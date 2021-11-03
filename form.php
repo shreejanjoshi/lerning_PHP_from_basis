e3f95e0a93aa6aeb5a182435883c19e7af84ed92
Shreejan Joshi <shreejanjoshi97@gmail.com>
2021-11-03 07:15:58 +0100

    building form page

---
+++ form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="form.php" method="POST">
+        <!-- action on same page because we will code here but normal it should in another page -->
+        <!-- method is the way it send its data -->
+
+        <input type="text" placeholder="Enter username">
+        <input type="password" placeholder="Your password">
+        <input type="submit">
+    </form>
+</body>
+
+</html>
