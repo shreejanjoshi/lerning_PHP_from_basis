0117786b993c0edb9335ac9c8e161d6ed199001c
Shreejan Joshi <shreejanjoshi97@gmail.com>
2021-11-04 04:20:17 +0100

    ade a array so specific people can login

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # lerning_PHP_from_basis
+
+
+### Hi Hi
+Hello everyone if you are wondering what hell is this then this is the place where I practice my PHP. 
+To be honest I am not good at PHP but I wanna see myself grow. 
+So, this the place where I will put my pratice PHP so that in future I can know how weak was I.
--- form.php
+++ form.php
@@ -2,18 +2,24 @@
 
 if (isset($_POST['submit'])) {
 
-    $name = ["Shreejan", "Student", "Jackson"];
+    $minimun = 5;
+    $maximun = 10;
+
+    $name = ["Shreejan", "Tommy", "Jackson", "Steven"];
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    if (strlen($username) < 5) {
+    if (strlen($username) < $minimun) {
         echo "Username must be longer";
-    } elseif (strlen($username) > 10) {
+    } elseif (strlen($username) > $maximun = 10) {
         echo "Username must not be longer then 10 char";
     }
 
-    if()
-
+    if(!in_array($username, $name)){
+        echo "Sorry you are not allowed";
+    }else{
+        echo "Welcome";
+    }
 
 }
 
